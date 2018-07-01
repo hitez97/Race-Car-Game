dcfde22cc26234514818cfa9c0ffb964df569aba
Hitesh Verma <hitz.rocker@gmail.com>
2018-07-01 11:30:02 +0530

    Crash bug fix.

--- RC.py
+++ RC.py
@@ -46,6 +46,7 @@ def crash():
 x =  (display_width * 0.45)
 y = (display_height * 0.8)
 x_change = 0
+y_change = 0
 thing_startx = random.randrange(0, display_width)
 thing_starty = -600
 thing_speed = 7
@@ -64,13 +65,18 @@ while not gameExit:
                 x_change = -5
             if event.key == pygame.K_RIGHT:
                 x_change = 5
+            if event.key == pygame.K_UP:
+                y_change = -5
+            if event.key == pygame.K_DOWN:
+                y_change = 5
 
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 x_change = 0
+                y_change = 0
 
     x += x_change
-
+    y += y_change
     gameDisplay.fill(white)
     things(thing_startx, thing_starty, thing_width, thing_height, black)
     thing_starty += thing_speed
@@ -84,8 +90,9 @@ while not gameExit:
     if y < thing_starty+thing_height:
         print('y crossover')
 
-    if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
+    if x > thing_startx and x < thing_startx + thing_width or x + car_width > thing_startx and x + car_width < thing_startx + thing_width:
             print('x crossover')
+            crash()
 
 
     pygame.display.update()
